521101db6e8635cfc6859757962e518c0078a54d
ThaliaFreitas <freitas.thalia97@gmail.com>
2024-10-24 11:15:24 -0300

    Initial Commit

---
+++ InfoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InfoApp
+//
+//  Created by Thalia on 24/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
